057b1a6410c2f9918f2d4c95e27db383a0cb4b17
elisa marques <elisamarques466@gmail.com>
2024-07-23 22:11:21 -0300

    alteracao

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', './src/views');
-
-app.use(express.static('./src/public'));
-
-//mostrando a pagina index.ejs
-app.get('/', function (request, response) {
-    response.render('index');
+const express = require("express");
+const path = require("path");
+const enableHotReload = require("./hot-reload");
+const app = express();
+// Configurações do seu app Express
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+console.log("Views path set to:", path.join(__dirname, "views"));
+// Configuração de pasta pública
+app.use(express.static(path.join(__dirname, "public")));
+console.log("Static files path set to:", path.join(__dirname, "public"));
+// Habilitar hot-reload
+enableHotReload(app);
+// Rotas
+app.get("/", (req, res) => {
+res.render("index");
 });
-
-
-//iniciando o servidor 
-app.listen(3000, function () {
-    console.log('Servidor rodando na porta 3000');
-});
+// Inicie o servidor
+const port = 3003;
+app.listen(port, () => {
+console.log(`Servidor rodando em http://localhost:${port}`);});
